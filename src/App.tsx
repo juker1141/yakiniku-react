1903c26b30d6d30bea9b6dfb9563d1ddda8ddf13
Ryu <juker1141@gmail.com>
2024-12-21 00:31:31 +0800

    try deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
 import EventDetail from "./pages/EventDetail";
